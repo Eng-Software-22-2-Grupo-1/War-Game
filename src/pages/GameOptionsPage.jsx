7ef5f438f88bb81758e4ac4dff513544961cecd2
Vitor Souza <vitor.a.de.souza@accenture.com>
2022-11-11 19:49:45 -0300

    chore: initial styling of the game options page

--- src/pages/GameOptionsPage.jsx
+++ src/pages/GameOptionsPage.jsx
@@ -1,43 +1,91 @@
+import { Box, Button, Container, MenuItem, Select } from '@mui/material';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+
+import WAR from '../assets/WAR.svg';
+import './GameOptionsPage.css';
 
 const GameOptionsPage = ({ onOptionsSubmit }) => {
   const [gameOptions, setGameOptions] = useState({
     numberOfPlayers: 3,
+    numberOfBots: 0,
     map: null,
     mode: null
   });
+
+  const [availableBots, setAvailableBots] = useState([...Array(gameOptions.numberOfPlayers).keys()]);
 
   const handleClick = () => {
     onOptionsSubmit(gameOptions);
   };
 
+
   return (
-    <>
-      <div>GameOptionsPage</div>
+    <Box className='options-background'>
+      <Container
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: "100px",
+          height: "100vh"
+        }}>
+        <Box
+          component="img"
+          sx={{
+            width: "50vh",
+            background: "antiquewhite",
+            padding: "15px",
+            borderRadius: "10px"
+          }}
+          alt="War logo."
+          src={WAR}
+        />
+        <Box style={{ display: "grid", alignItems: "center" }}>
+          <p style={{ color: "white", fontWeight: "bold" }}>Quantidade de Jogadores</p>
+          <Select
+            style={{ width: "200px", backgroundColor: "white", fontWeight: "bold" }}
+            id="demo-simple-select-helper"
+            defaultValue={gameOptions.numberOfPlayers}
+            onChange={(e) => {
+              const options = gameOptions;
 
-      <form>
-        <select
-          name="numberOfPlayers"
-          id="numberOfPlayers"
-          defaultValue={3}
-          required
-          onChange={(e) => {
-            const options = gameOptions;
+              options.numberOfPlayers = parseInt(e.target.value);
 
-            options.numberOfPlayers = parseInt(e.target.value);
+              setGameOptions(options);
+              setAvailableBots([...Array(options.numberOfPlayers).keys()]);
+              console.log(gameOptions);
+            }}
+          >
+            <MenuItem value={3}>3</MenuItem>
+            <MenuItem value={4}>4</MenuItem>
+            <MenuItem value={5}>5</MenuItem>
+            <MenuItem value={6}>6</MenuItem>
+          </Select>
+          <p style={{ color: "white", fontWeight: "bold" }}>Quantidade de Bots</p>
+          <Select
+            style={{ width: "200px", backgroundColor: "white", fontWeight: "bold" }}
+            id="demo-simple-select-helper"
+            defaultValue={gameOptions.numberOfBots}
+            onChange={(e) => {
+              const options = gameOptions;
 
-            setGameOptions(options);
-          }}>
-          <option value={3}>3</option>
-          <option value={4}>4</option>
-          <option value={5}>5</option>
-          <option value={6}>6</option>
-        </select>
+              options.numberOfBots = parseInt(e.target.value);
 
-        <input type="submit" onClick={handleClick} />
-      </form>
-    </>
+
+              setGameOptions(options);
+              console.log(gameOptions);
+
+            }}
+          >
+            {availableBots.map(e => {
+              return <MenuItem key={e} value={e}>{e}</MenuItem>
+            })}
+          </Select>
+          <Button style={{ width: "200px", height: "60px", marginTop: "30px", backgroundColor: "#7D1815" }} variant="contained" disableElevation onClick={handleClick}>Jogar</Button>
+        </Box>
+      </Container >
+    </Box>
   );
 };
 
